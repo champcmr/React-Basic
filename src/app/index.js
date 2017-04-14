8e98217918bfb7f5074427b7a3620e01decfcb1b
champcmr <chandrakant@jumpbyte.com>
2017-04-14 16:22:51 +0530

    Displsy member list.

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Clock from './component/clock.component.js';
+import Clock from './clock.component.js';
+import Members from './members/component/members.component.js';
 
 class App extends React.Component{
     render(){
         return(
-            <div className="container">
+            <div className="">
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">Todos</a>
+                    </div>
+                    <ul className="nav navbar-nav">
+                        <li className="active"><a href="#">Members</a></li>
+                        {/*<li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>*/}
+                    </ul>
+                </div>
+            </nav>
+            <div className="container-fluid">
                 <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1"> 
-                        <h1>Hello</h1>
-                        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-                        <Clock name="chandresh" />
+                    <div className="col-xs-6 text-left">
+                        <h3 className="p-0 m-0 page-header">Members List</h3>
+                    </div>
+                    <div className="col-xs-6 text-right">
+                        <button className="btn btn-primary">Add Member</button>
+                    </div> 
+                </div>
+                <div className="row">
+                    <div className="col-xs-12">                         
+                        <Members name="xxx"/>
                     </div>
                 </div>
             </div>
-            
+            </div>
         );
 
     }
